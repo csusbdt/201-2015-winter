e6fc70f0cffa8f270cb557c538c8fc486e7ec7c6
David Turner <csusbdt@gmail.com>
2015-02-16 13:32:15 -0800

    X

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
     ['E1', 200, new Date('2015-02-09T13:10:00'), '<a href="E1.html">E1: Exam 1</a>'],
     ['L4',  40, new Date('2015-02-13T23:59:59'), '<a href="L4.html">L4: Functions</a>'],
     ['L5',  60, new Date('2015-02-20T23:59:59'), '<a href="L5.html">L5: Classes</a>'],
-    ['E2', 200, new Date('2015-02-23T23:59:59'), '<a href="E2.html">E2: Exam 2</a>'],
+    ['E2', 200, new Date('2015-02-25T23:59:59'), '<a href="E2.html">E2: Exam 2</a>'],
     ['L6',  50, new Date('2015-02-27T23:59:59'), '<a href="L6.html">L6: Arrays</a>'],
     ['L7',  50, new Date('2015-03-06T23:59:59'), '<a href="L7.html">L7: Binary Search</a>'],
     ['L8',  40, new Date('2015-03-13T23:59:59'), '<a href="L8.html">L8: Sorting</a>'],
